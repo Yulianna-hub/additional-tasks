7145394bcea22be2674e64de11a304b8254d15ef
Yuliia Borys <yuliannaboris@gmail.com>
2020-03-30 10:58:20 +0200

    On branch lesson02

--- lesson02/script.js
+++ lesson02/script.js
@@ -1,3 +1,20 @@
 'use strict';
 
-let = 266219;
+let num = 266219;
+let result = 1;
+num = String(num);
+
+for (let i = 0; i < num.length; i++){
+    result *= num[i];
+};
+console.log(result);
+
+result **= 3;
+console.log(result);
+
+result = String(result);
+document.getElementById('paragraph').innerHTML = (result.substr(0, 2));
+
+
+
+
